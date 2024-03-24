b619c02c6cfbee03641381ff586f48f953f25eaa
Sifu-Tech <sifucomputeracademy@gmail.com>
2024-03-24 03:43:11 +0300

    first commit

---
+++ week1 C++/Day1/drawing_shapes/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //drawing a rectangle
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+
+    return 0;
+}
